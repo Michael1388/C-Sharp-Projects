e52a31a86c82cbd73297509fad62b620258ac871
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-03-09 14:21:15 -0600

    update Basic C# Program file
    Upload assignment file "Acme Accounting Systems Folder and update Basic C# Program file

---
+++ Basic C# Programs/Hello World/Hello World/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Hello, World! The use of double quotes\" \" is necessary");
+        Console.ReadLine();
+        string doubleQuotes = "The use of double quotes\" \" is necessary";
+        }
+    }
