3bedbaeb14bd63865bd19adc42bfd0012ac7e307
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-03-10 14:04:17 -0600

    Update C# Folder- Submit DR Assignment
    Update C# Folder- Submit Tech Academy DR Assignment

---
+++ Basic C# Programs/TADailyReport/TADailyReport/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TADailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Purpose: To create a daily report(DR) for students of The Tech Academy.
+
+            Console.WriteLine("The Tech Academy\nStudent Daily Report"); //write to screen
+            Console.WriteLine("What is your name?"); //write to screen
+            string yourName = Console.ReadLine(); //stores user input in variable yourName of data type string
+
+            Console.WriteLine("What course are you on?");//write to screen
+            string courseName = Console.ReadLine(); //stores user input in variable courseName of data type string
+
+            Console.WriteLine("What page number?"); //write to screen
+            string pageNumber = Console.ReadLine(); //stores user input in variable pageNumber of data type string
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");//write to screen
+            string needHelp = Console.ReadLine();   //stores user input in variable needHelp of data type string
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");   //write to screen
+            string positiveExperiences = Console.ReadLine();  //stores user input in variable positiveExperiences of data type string  
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");//write to screen
+            string otherFeedback = Console.ReadLine();  //stores user input in variable otherFeedback of data type string  
+
+            Console.WriteLine("How many hours did you study today?");//write to screen
+            string hoursStudied = Console.ReadLine();   //stores user input in variable hoursStudied of data type string 
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");//write to screen
+            Console.ReadLine(); // used to continue screen being in active state allowing ability to read before program automatically closes the console.
+
+        }
+
+    }
+}
---
+++ Basic C# Programs/VariablesAndDataTypes/VariablesAndDataTypes.cs/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BLOCK COMMENT = CTRL+K+C
+
+            // This is a simple program
+
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //CASTING example string to int
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine(); // example began int "favoriteNumber" BUT can't bc Readline is string command, so CASTING, change to string "favoriteNumber" then...
+            //int favNum = Convert.ToInt32(favoriteNumber); // CAST string to INT then we can add other numbers to it
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //DATA TYPES IN C#
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char randomLetter = 'X';
+            char questionMark = '\u2103'; // u2103 is unicode for ?
+            
+            decimal moneyInBank = 100.5m; // "deciaml" requires m after the amount ex 100.5m for compliler sake 
+            double heightInCentimeters = 211.30203092;
+            float secondsLeft = 2.62f; // float must use f after 
+
+            short temperaturesOnMars = -341;
+
+            string myName = "Michael";
+
+            // CAST or convert from int to string
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            //convert bool to string
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
