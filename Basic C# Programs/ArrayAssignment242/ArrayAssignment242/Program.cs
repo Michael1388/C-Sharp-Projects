4c06220e5c98536d6ced0a84a87e1d6cc333c499
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-03-16 16:00:33 -0500

    Update C# Folder: Add Array Assignment Step 242

---
+++ Basic C# Programs/ArrayAssignment242/ArrayAssignment242/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArrayAssignment242
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create a one-dimensional Array of strings. Ask the user to
+            // select an index of the Array and then display the string at that index on the screen.
+            // Add in a message that displays when the user selects an index that doesn’t exist.
+
+            Console.WriteLine("Enter a number between 0 and 5 to view the contents of that memory slot. "); // write user input query to console
+            string userinput1 = Console.ReadLine();    // store user input in string variable "userinput1"
+            int viewNum1 = Convert.ToInt32(userinput1);   // convert string "userinput1" to int(eger) and store as variable "viewNum1" 
+            string[] stringArray1 = { "Egg", "Yolk", "Green Onions", "Chicken Broth", "Ginger", "Garlic" };  // create string array and store in variable "stringArray1"
+
+            if (viewNum1 > 5)   //if statement returns comment if user selects an index that doesn’t exist
+            {
+                Console.WriteLine("Choose a different number, below 6 please.");   // write to console for user to choose a different number 
+                viewNum1 = Convert.ToInt32(Console.ReadLine());   // store new choice in variable "viewNum1"
+
+            }
+
+            Console.WriteLine(stringArray1[viewNum1]); // write user choice to console
+
+
+
+            // Create a one-dimensional Array of integers. Ask the user to select an index
+            //of the Array and then display the integer at that index on the screen.
+            // Add in a message that displays when the user selects an index that doesn’t exist.
+
+            Console.WriteLine("Enter a number between 0 and 7 to view that memory slot. ");     // write user input query to console
+            string userinput2 = Console.ReadLine();  // store user input in string variable "userinput2"
+            int viewNum2 = Convert.ToInt32(userinput2);  // convert string "userinput2" to int(eger) and store as variable "viewNum2" 
+            int[] numArray2 = { 100, 32, 15, 200, 525, 60, 88, 575 };  // create integer array and store in variable "numArray2"
+
+            if (viewNum2 > 7)   //if statement returns comment if user selects an index that doesn’t exist
+            {
+                Console.WriteLine("Choose a different number, below 8 please.");    // write to console for user to choose a different number 
+                    viewNum2 = Convert.ToInt32(Console.ReadLine()); // store new choice in variable "viewNum2"
+
+            }
+
+            Console.WriteLine(numArray2[viewNum2]); // write user choice to console
+
+
+            //Create a list of strings. Ask the user to select an index of the list and then
+            //display the content at that index on the screen.
+
+            Console.WriteLine("Enter a number between 0 and 6 to view the TEXT contained in that memory slot. ");   // write user input query to console
+            string userinput3 = Console.ReadLine(); // store user input in string variable "userinput3"
+            int viewNum3 = Convert.ToInt32(userinput3); // convert string "userinput3" to int(eger) and store as variable "viewNum3"
+            List<string> stringList = new List<string>(); // create list store as variable "stringList
+            stringList.Add("Hello"); //add string content
+            stringList.Add("I");    //add string content
+            stringList.Add("am");
+            stringList.Add("Michael, ");
+            stringList.Add("a");
+            stringList.Add("real");
+            stringList.Add("human.");
+
+
+            Console.WriteLine(stringList[viewNum3]); // write user choice to console
+
+
+            Console.ReadLine(); // keeps console open until closed by user....// && this concludes our program for today, thank you for participating :)
+
+
+        }
+    }
+}
---
+++ Basic C# Programs/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic; // for Lists
+
+
+class Program
+{
+    static void Main()
+    {   //Array basics - arrays are limited whereas lists are more broad use.
+        //You will usually use them when you have a fixed quantity that you want to group together
+        //*** that you know isn't going to change bc in an array, once you set the size it is difficult to change it
+        int[] numArray = new int[5];
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 }; // simplified version of the above code
+
+        int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 }; // YET ANOTHER EVEN MORE simplified version of the above code
+
+        numArray2[5] = 650;
+
+        // you can use what is called a byte array for very large objects
+        // byte[] byteArray = new byte[5000];
+
+        Console.WriteLine(numArray[3]);
+        Console.WriteLine(numArray1[3]);
+        Console.WriteLine(numArray2[5]);
+
+        //Lists || using System.Collections.Generic;
+
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(4);
+
+        Console.WriteLine(intList[0]);
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello");
+        stringList.Add("Michael");
+        //stringList.Remove("Hello");
+
+        Console.WriteLine(stringList[0]);
+
+        Console.ReadLine();
+            
+    }
+}
