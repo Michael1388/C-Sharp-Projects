d21523d546fe4a167a41200c6ae1d42765da236a
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-03-21 19:18:04 -0500

    Update C# folder. Add Iterate 6 Assignment

---
+++ Basic C# Programs/IterateArray6Assignment250/IterateArray6Assignment250/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1 Iterate through a string array and add user input to the end of each array string
+
+        Console.WriteLine("Enter your name: "); // write to screen, query user for input
+        string uName = Console.ReadLine(); // store user input in string variable "uName"
+
+        string[] names = { "Michael", "Jesse", "Erik" }; // create sting array 
+
+        for (int i = 0; i < names.Length; i++)  // for loop to iterate through the string array
+        {
+            if (names[i] == names[i]) // if statement to call the values of array
+            {
+                Console.WriteLine(names[i] + " + " + uName); // write to console contents of variable "names"
+                                                             // and integer var "i" or  "names[i]" plus variable "uName" 
+            }
+        }
+
+
+        // 2 Create and infinte loop
+        int x = 1;
+        while (x > 2)// infinite loop corrected from
+                     // (x == 1) which is an infinite loop
+        {
+            Console.WriteLine("ahhhhhhhhhhhh!!!!!!!!!!!!!!!!"); //if  infinite loop is run, this prints to screen
+
+        }
+
+
+        //3.Loop comparisons
+
+        //3.1 interate through an integer array and compare with < operator
+
+        int[] randomNumbers = { 40, 99, 85, 70, 82, 34, 91, 90, 16 }; //int array instantiated
+
+
+        for (int r = 0; r < randomNumbers.Length; r++) // for loop iterating through array
+        {
+            Console.WriteLine("< loop " + randomNumbers[r]); // print to console those values less than 85
+        }
+
+        // 3.3 interate through an integer array and compare with <= operator
+
+
+        for (int j = 0; j <= 8; j++)  // for loop iterating through array
+        {
+            Console.WriteLine("<= loop " + randomNumbers[j]); // print to console those values determined in if statement
+        }
+
+
+        // 4. List of unique strings
+
+        Console.WriteLine("Check if your favorite fruit is availabile: "); // user input
+        string userfruit = Console.ReadLine(); // user input stores in "userfruit"
+        userfruit = userfruit.ToLower(); // convert to lower to match case in list array
+
+        List<string> fruitInventory = new List<string>() { "apple", "peach", "orange", "kiwi", "banana" }; // list of values in string list
+
+        bool found = false; // set value of found to false
+        foreach (string value in fruitInventory) // interate through string
+        {
+            if (value == userfruit) // if user input matches value in List string "fruitInventory"
+            {
+                found = true; // if match is found, "found = true"
+                break; // program ends if found and continues to next loop
+            }
+        }
+
+
+        if (found) // if found is true
+        {       // console writes to screen and displays user's choice and matching index of list item in upper case
+            Console.WriteLine(userfruit.ToUpper() + ": Our inventory number is " + fruitInventory.IndexOf(userfruit) + ", and " + userfruit.ToUpper() + " is available!");
+        }
+        else // otherwise tells user that the text entered in not (in the list) available
+        {
+            Console.WriteLine("You've entered a fruit that is not available.");
+        }
+
+
+
+        // 5. Two identical Strings 
+
+        // 1 list of strings -- DONE
+        // 2 get user input -- DONE
+        // 3 create a lopp that iterates though 1 -- DONE
+        // 4 while in that loop get the element number of found matches comparing 2 --DONE
+        // 5 remove any break statements that prevent code from returning multiple matches --DONE
+        // 6 add code to loop to check to see if user put text in that isn't in the list --DONE
+        // 7 if user did 6 imform user input not in list --DONE
+
+
+        Console.WriteLine("Check availability: "); // user input
+        string userinput = Console.ReadLine(); // user input is stored in var "userfruit"
+        userinput = userinput.ToLower(); // convert to lower to match case in list 
+
+        List<string> fruitList = new List<string>() { "apple", "grape", "apple", "kiwi", "banana" }; // list of values in string list
+        bool founditem = false; // set value of found to false
+        string indices = ""; // create empty variable to store matches during iteration
+
+        for (int f = 0; f < fruitList.Count; f++) // iterate through the list
+        {
+            if (fruitList[f] == userinput) // if user userinput matches index in fruitlist assign it to f
+            {
+                founditem = true;   // create true state in bool 
+                indices += f + " ";     //add matching index items to variable indices
+
+            }
+
+        }
+
+        if (!founditem) // if userinput item not found 
+        {
+
+            Console.WriteLine("You've entered an item that is not available."); // tell user not found
+
+        }
+        else
+        {
+            Console.WriteLine(indices); // print index number of found items requested
+        }
+
+
+        // 6.
+
+        // 1. Create list with two identical strings
+        // 2. Create foreach loop that evaluates whether each item is unique or a duplicate
+        // 3. Print results to console
+
+        List<string> dupNames = new List<string>() { "Josh", "Ethan", "Chris", "Holly", "Allie", "Josh", "Ethan" }; // list with duplicate entries
+        List<string> namesUnique = new List<string>(); // new list to contain sorted items from "names" list
+
+        foreach (string item in dupNames) // iterate through list "name" store list item in variable "item" 
+        {
+            if (!namesUnique.Contains(item)) // checks if "item" is not contained in "namesUnique"
+            {
+                namesUnique.Add(item); // adds "item" to "namesUnique" 
+                Console.WriteLine("These items are unique: " + item); // prints unique items to console
+
+            }
+            else // else
+            {
+                Console.WriteLine("This item is a duplicate: " + item); // prints duplicate items to console
+            }
+        }
+
+        //Console.WriteLine(string.Join(",", namesUnique)); // checked condition of if statement to view results while writing code 
+
+
+
+        Console.ReadLine(); // keeps window open until closed by user or enter / return key is pressed again
+    }
+}
+
+
--- Basic C# Programs/Iteration/Iteration/Program.cs
+++ Basic C# Programs/Iteration/Iteration/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
-    {   
+    {
         //interate through an integer array
         int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
 
@@ -17,22 +17,218 @@ class Program
             {
                 Console.WriteLine("Passing test score: " + testScores[i]);
             }
-            
+
         }
-        
+
         //now through a string
 
         string[] names = { "Jesse", "Erik", "Daniel", "Adam", "Michael" };
 
         for (int j = 0; j < names.Length; j++)
         {
-            if (names[j] == "Michael")
+            //if (names[j] == "Michael") // comment out to see full list of names/ write to console if needed
             {
                 Console.WriteLine(names[j]);
             }
         }
-        Console.ReadLine();
+
+
+        int[] numberArray = { 300, 75, 420, 380, 225 };
+
+        for (int i = 0; i < numberArray.Length; i++)
+        {
+            if (numberArray[i] / 2 >= 150)
+            {
+                Console.WriteLine(numberArray[i] + " divided by 2 is greater than or equal to 150");
+            }
+        }
+
+        //returns:
+        //300 divided by 2 is greater than or equal to 150
+        //420 divided by 2 is greater than or equal to 150
+        //380 divided by 2 is greater than or equal to 150
+
+
+        // LISTS
+
+        //LIST - INT
+
+        List<int> testResults = new List<int>();
+        testResults.Add(98);
+        testResults.Add(99);
+        testResults.Add(81);
+        testResults.Add(72);
+        testResults.Add(70);
+
+        foreach (int result in testResults)
+        {
+            if (result > 85)
+            {
+                Console.WriteLine("Passing test score result: " + result);
+            }
+        }
+
+
+        //LIST - STRING
+        List<string> namesString = new List<string>() { "Michael", "Joshua", "Ethan", "Holly" };
+
+        foreach (string userName in namesString)
+        {
+            //if (userName == "Holly")
+            {
+                Console.WriteLine(userName);
+            }
+        }
+
+
+        // Adding something to something??....
+        List<int> testResults1 = new List<int>() { 98, 99, 12, 74, 23, 99 };
+        List<int> passingScores = new List<int>(); // new list to contain var "passingScores"
+
+        foreach (int results in testResults1)
+        {
+            if (results > 85)
+            {
+                //Console.WriteLine("Passing test score result: " + results); to view results > 85
+                passingScores.Add(results); // Add to new list "passingScores" "results" > 85
+            }
+        }
+        Console.WriteLine(passingScores.Count); // to count results and write output to screen
+
+
+
+        //JOsh example 
+
+        int x = 0;
+        Console.WriteLine("x is: " + x);
+        Console.WriteLine("x after prepend is: " + ++x);
+        Console.WriteLine("x postpend is: " + x++);
+        Console.WriteLine("x post postpend is: " + x);
+
+
+        //// ?? uncecessary code commented out 
+        ////Console.WriteLine(fruitInventory.IndexOf(userfruit));
+
+        ////if (fruitInventory.IndexOf(userfruit) != -1)
+        ////{
+        ////    Console.WriteLine("You chose: " + userfruit + ": Our inventory number is " + fruitInventory.IndexOf(userfruit) + ", and " + userfruit + " is available!");
+        ////}
+        ////else
+        ////{
+        ////    Console.WriteLine("You've entered a fruit that is not available.");
+        ////}
+        //// ?? END uncecessary code commented out 
+        ///
+
+        // 3 Loop comparisons
+
+        // interate through an integer array and compare with < operator
+
+        int[] randomNumber = { 40, 99, 85, 70, 82, 34, 91, 90, 16 }; //int array instantiated
+
+        for (int e = 0; e < randomNumber.Length; e++) // for loop iterating through array
+        {
+            if (randomNumber[e] < 80) // if statement comparing iteration of "randomNumber" to < 85
+            {
+                Console.WriteLine(randomNumber[e]); // print to console those values less than 85
+            }
+
+        }
+
+        // interate through an integer array and compare with <= operator
+
+        int[] numArray = { 250, 199, 30, 375, 201, 75 };   //int array instantiated
+
+        for (int g = 0; g < numArray.Length; g++)  // for loop iterating through array
+        {
+            if (numArray[g] / 2 >= 75)  // if statement comparing iteration to >= 75
+            {
+                Console.WriteLine(numArray[g]); // print to console those values determined in if statement
+            }
+        }
+
+        //5.
+        Console.WriteLine("Check availability: "); // user input
+        string userid = Console.ReadLine(); // user input is stored in var "userfruit"
+        userid = userid.ToLower(); // convert to lower to match case in list 
+
+        List<string> fruitList = new List<string>() { "apple", "grape", "apple", "kiwi", "banana" }; // list of values in string list
+
+
+        List<string> familyList = new List<string>() { "Josh", "Josh", "Josh", "Josh", "Allie", "Holly", "Ethan", "Chris" }; // list of values in string list
+
+        //for (declare variable used for itterating ; if statement that determines if the loop can continue ;  amount to increase variable by each itteration )
+
+        // write a for loop that checks each element of the list to find an element that contains 'Chris' then print out its index
+        string indexes = "";
+        for (int a = 0; a < familyList.Count; a++)
+        {
+            if (familyList[a] == "Chris")
+            {
+                indexes += a + "";
+            }
+        }
+        Console.WriteLine("Chris = " + indexes);
+
+        // write a for loop that finds each instance of "Josh" and removes them, then prints the new list
+
+
+        for (int b = 0; b < familyList.Count; b++)
+        {
+            if (familyList[b] == "Josh")
+            {
+                familyList.RemoveAt(b);
+            }
+        }
+        string output = string.Join(",", familyList);
+
+        Console.WriteLine(output);
+
+        // ABOVE IS WRITTEN CORRECT EXCEPT THAT IT LEAVES DATA BECAUSE IT HAS TO BE WRITTEN IN REVERSE AS SHOWN BELOW
+
+        for (int b = familyList.Count - 1; b >= 0; b--)
+        {
+            if (familyList[b] == "Josh")
+            {
+                familyList.RemoveAt(b);
+            }
+        }
+
+        output = string.Join(",", familyList); //could eliminate var and..
+        Console.WriteLine(output); //just use inside of ();(string.Join(",", familyList);
+
+        //for (declare variable used for itterating (int i = 0;); if statement that determines if the loop can continue (i < length);  amount to increase variable by each itteration (i++)  )
+
+
+        string indices = "";
+        int k = 0;
+        
+            if (k < fruitList.Count) // 161
+        {
+            if (fruitList[k] == userid)
+            {
+                indices += k + " ";
+            }
+            k++;
+            
+        }
+        Console.WriteLine(indices);
+
+        int m = 0;
+        while (m < fruitList.Count)
+        {
+
+            if (fruitList[m] == userid)
+            {
+                indices += m + " ";
+            }
+            m++;
+        }
+
+        Console.ReadLine(); // keeps window open until closed by user
 
     }
+
 }
+
 
