dae2425eeb005cd1bdbd962f0b7b01965e5376b5
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-03-24 19:50:42 -0500

    Add Method Assignment 281

---
+++ Basic C# Programs/MethodAssignment281/MethodAssignment281/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment281
+{
+    class Program
+    {
+
+
+        //1. Create a class. In that class, create a void method that takes two integers as parameters. --Done
+        //a. Have the method do a math operation on the first integer and display the second integer to the screen. --Done
+
+        //2. In the Main() method of the console app, instantiate the class. --Done
+
+        //3. Call the method in the class, passing in two numbers.--Done
+
+        //4. Call the method in the class, specifying the parameters by name. --Done
+
+        //5. Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code. --Done
+
+        static void Main(string[] args)
+        {
+
+            OptionalParameters name1 = new OptionalParameters(); // instantiated Class 
+            name1.OpParam(1, 2); // called method passing in two numbers, specifying parameters by name
+           
+
+
+            Console.ReadLine(); // keep console open to view program until closed by user or return/enter key
+        }
+    }
+}
