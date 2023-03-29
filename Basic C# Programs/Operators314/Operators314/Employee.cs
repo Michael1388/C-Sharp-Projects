ea4d319e592d26cc3841ad969367915f63b6a582
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-03-29 11:40:02 -0500

    Update C# Folder Add Operators314 assignment

---
+++ Basic C# Programs/Operators314/Operators314/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators314
+{
+    //Create an Employee class with Id, FirstName and LastName properties.  --Done
+
+    //In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property. Remember that comparison operators must be overloaded in pairs. --Done
+    //... meaning I'd have to use != also
+
+    //In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their properties.Then compare the two Employee objects using the newly overloaded operators and display the results. --Done
+
+    //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+
+    public class Employee   //new public Class Employee 
+    {
+        public int Id { get; set; } // public int class property Id
+        public List<string> FirstName { get; set; } // public property FirstName of string type 
+        public List<string> LastName { get; set; }  // public property LastName of string type 
+
+
+     
+        public static bool operator ==(Employee id1, Employee id2) // overloading operator ==
+        {
+            return (id1.Id == id2.Id); // returns boolean result of equals to objects compared
+
+            // same as above written differently for my reference later
+            //{
+            //    return true;
+            //}
+
+            //return false;
+
+
+        }
+        public static bool operator !=(Employee id1, Employee id2) // overloading operator !=
+        {
+            return (id1.Id != id2.Id);  // returns boolean result of not equals to objects compared
+
+            // same as above written differently for my reference later
+            //if (id1.Id != id2.Id)
+            //{
+            //    return true;
+            //}
+
+
+            //return false;
+
+        }
+    }
+}
---
+++ Basic C# Programs/Operators314/Operators314/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators314
+{
+
+    //Create an Employee class with Id, FirstName and LastName properties. --Done
+
+    //In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property. Remember that comparison operators must be overloaded in pairs. --Done
+
+    //In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their properties.Then compare the two Employee objects using the newly overloaded operators and display the results. --Done
+
+    //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();     //Instantiate object
+            Employee employee2 = new Employee();    //Instantiate object 2
+
+            employee.FirstName = new List<string>() { "Mickey" }; // initialize Employee object
+            employee.LastName = new List<string>() { "Mouse" }; // initialize Employee object
+            employee.Id = 1;    //add value of 1 to employee property 
+
+
+            employee2.FirstName = new List<string>() { "Minnie" }; // initialize Employee object
+            employee2.LastName = new List<string>() { "Moose" }; // initialize Employee object
+            employee2.Id = 2;   //add value of 2 to employee2 property
+
+          
+
+
+            Console.WriteLine((employee == employee2)); // display result of if equals, could also return if not equal !=
+            
+            Console.ReadLine(); // keep console window open
+
+        }
+       
+    }
+}
