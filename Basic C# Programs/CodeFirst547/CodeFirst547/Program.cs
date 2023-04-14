b2f64c82bf4bfcf64e9b6fc787542b52249b63e2
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-04-14 13:59:11 -0500

    Add CodeFirst547

---
+++ Basic C# Programs/CodeFirst547/CodeFirst547/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CodeFirst547
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+
+        }
+    }
+}
